f253ea6800e43a3fa5089196aa4bcfdb5dfbe3bd
Luísa Marques <137580816+luhmarquz@users.noreply.github.com>
2023-12-03 15:39:07 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,128 @@
+
+const modal0 = document.getElementById("modal0");
+const modal1 = document.getElementById("modal1");
+const modal2 = document.getElementById("modal2");
+const modal3 = document.getElementById("modal3");
+const modal4 = document.getElementById("modal4");
+const modal5 = document.getElementById("modal5");
+const modal6 = document.getElementById("modal6");
+const modal7 = document.getElementById("modal7");
+const modal8 = document.getElementById("modal8");
+const modal9 = document.getElementById("modal9");
+
+
+
+//todas
+const fechar = document.querySelectorAll('.fechar')
+const palavra = document.querySelectorAll(".palavra")
+
+//0
+palavra[0].addEventListener('click',() =>{
+    modal0.style.display = 'block'
+})
+ 
+fechar[0].addEventListener('click', ()=>{
+    modal0.style.display= 'none'
+})
+
+
+
+//1
+palavra[1].addEventListener('click',() =>{
+    modal1.style.display = 'block'
+})
+ 
+fechar[1].addEventListener('click', ()=>{
+    modal1.style.display= 'none'
+})
+
+
+
+//2
+palavra[2].addEventListener('click',() =>{
+    modal2.style.display = 'block'
+})
+ 
+fechar[2].addEventListener('click', ()=>{
+    modal2.style.display= 'none'
+})
+
+
+
+//3
+palavra[3].addEventListener('click',() =>{
+    modal3.style.display = 'block'
+})
+ 
+fechar[3].addEventListener('click', ()=>{
+    modal3.style.display= 'none'
+})
+
+
+//4
+palavra[4].addEventListener('click',() =>{
+    modal4.style.display = 'block'
+})
+ 
+fechar[4].addEventListener('click', ()=>{
+    modal4.style.display= 'none'
+})
+
+   
+
+//5
+palavra[5].addEventListener('click',() =>{
+    modal5.style.display = 'block'
+})
+ 
+fechar[5].addEventListener('click', ()=>{
+    modal5.style.display= 'none'
+})
+
+
+
+//6
+palavra[6].addEventListener('click',() =>{
+    modal6.style.display = 'block'
+})
+ 
+fechar[6].addEventListener('click', ()=>{
+    modal6.style.display= 'none'
+})
+
+
+
+//7
+palavra[7].addEventListener('click',() =>{
+    modal7.style.display = 'block'
+})
+ 
+fechar[7].addEventListener('click', ()=>{
+    modal7.style.display= 'none'
+})
+
+
+
+//8
+palavra[8].addEventListener('click',() =>{
+    modal8.style.display = 'block'
+})
+ 
+fechar[8].addEventListener('click', ()=>{
+    modal8.style.display= 'none'
+})
+
+
+
+//9
+palavra[9].addEventListener('click',() =>{
+    modal9.style.display = 'block'
+})
+ 
+fechar[9].addEventListener('click', ()=>{
+    modal9.style.display= 'none'
+})
+
+
+
+
